fbf5e9fdff5194ed2bfed0643029b3a5ff7d3f44
AgentG010 <georgetroulis@gmail.com>
2016-10-01 10:17:57 -0700

    Solution Code: Added Buttons

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,8 +1,22 @@
+#include "WPILib.h"
+#include "Commands/Shooter/ShootBall.h"
+#include "Commands/Shooter/BrakeShooterMotor.h"
+#include "Commands/Intake/IntakeBall.h"
+#include "Commands/Intake/BrakeIntakeMotor.h"
+
+
 #include "OI.h"
 
 OI::OI()
 	: driveStick(0)
 {
+	JoystickButton* shootButton = new JoystickButton(driveStick, 1);
+	shootButton->WhenPressed(new ShootBall());
+	shootButton->WhenReleased(new BrakeShooterMotor());
+
+	JoystickButton* intakeButton = new JoystickButton(driveStick, 2);
+	intakeButton->WhenPressed(new IntakeBall());
+	intakeButton->WhenReleased(new BrakeIntakeMotor());
 }
 
 Joystick* OI::getDriveStick()
